2435ba7deb436bc6e6f96b4753e7987fe0c1c988
Clarent Rowe <clarentr@winningmoves.com>
2013-09-20 15:26:30 +0100

    initialise new repo

--- jQuery.showHideTableColumns.js
+++ jQuery.showHideTableColumns.js
@@ -1,5 +1,5 @@
 /* 
- * version 1.5
+ * version 2.2
  * 
  * showHideColumns jQuery plugin
  * 
@@ -21,38 +21,46 @@
             width:'100%',
             fontSize:'13px',
 			delivery:'download',
-			showCSVButton:true
+			showCSVButton:true,
+			showShowHide:true
         }, options );
         
         var choiceDiv = this;
-		tableName = this[0].className;
-        $this = choiceDiv.selector;
-        divName = settings.selectorClass;
-		buttonClass = settings.csvButtonClass;
+		var tableName = this[0].className;
+        var $this = choiceDiv.selector;
+        var divName = settings.selectorClass;
+		var buttonClass = settings.csvButtonClass;
+		var showButton = settings.showCSVButton;
+		var showSH = settings.showShowHide;
         
         var string = "<div class='"+divName+"'>";
-		if(settings.showCSVButton){
+		
+		if(showButton){
 			string += "<a href='#' id='"+tableName+"' class= '"+buttonClass+"'>Download CSV</a>";
 		}
-		string += "<table style='width:"+settings.width+";'><tr><td><strong>Show Hide Column  -</strong></td>";
-        
-        $($this+' th').each(function( index, listItem ) {
-            
-            if(settings.bold){
-                title = '<strong>'+listItem.outerText+'</strong>';
-            }else{
-                title = listItem.outerText;
-            }
-            value = index+1;
-            
-            string += "<td style='font-size:"+settings.fontSize+"'><label><input type='checkbox' name='tableName' value='"+value+"' class='tableName' checked='checked'>"+title+"</label></td>";
-        });
-    
-        string += "</tr></table></div>";
+		
+		if(showSH){
+			string += "<table style='width:"+settings.width+";'><tr><td><strong>Show Hide Column  -</strong></td>";
+			
+			$($this+' th').each(function( index, listItem ) {
+				
+				if(settings.bold){
+					title = '<strong>'+listItem.outerText+'</strong>';
+				}else{
+					title = listItem.outerText;
+				}
+				value = index+1;
+				
+				string += "<td style='font-size:"+settings.fontSize+"'><label><input type='checkbox' name='tableName' value='"+value+"' class='tableName' checked='checked'>"+title+"</label></td>";
+			});
+		
+			string += "</tr></table>";
+		}
+		string += "</div>";
         
         $(string).insertBefore($this);
         
-		if(settings.showCSVButton){
+		if(showButton){
 			$('#'+tableName).click(function(){
 			   var button = $(this);
 			   var id = button.attr('id');
@@ -73,20 +81,22 @@
 		   });
 		}
         
-        temp = divName.split(' ');
-        firstClass = temp[0];
- 
-        return this.prev("."+firstClass).find('input').change(function(){
-            
-            val = parseInt($(this).val());
-            checked = $(this).is(':checked');
-            
-            if(checked){
-                $($this+' td:nth-child('+val+'), '+$this+' th:nth-child('+val+')').fadeIn('slow');
-            }else{
-                $($this+' td:nth-child('+val+'), '+$this+' th:nth-child('+val+')').fadeOut('slow');
-            }
-        });
+		if(showSH){
+			temp = divName.split(' ');
+			firstClass = temp[0];
+	 
+			return this.prev("."+firstClass).find('input').change(function(){
+				
+				val = parseInt($(this).val());
+				checked = $(this).is(':checked');
+				
+				if(checked){
+					$($this+' td:nth-child('+val+'), '+$this+' th:nth-child('+val+')').fadeIn('slow');
+				}else{
+					$($this+' td:nth-child('+val+'), '+$this+' th:nth-child('+val+')').fadeOut('slow');
+				}
+			});
+		}
         
     };
     
